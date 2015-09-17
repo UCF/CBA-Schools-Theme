7849479a6f47584f4e25b63a1c1bd9fd42de7c91
Jim Barnes <james.barnes@ucf.edu>
2015-09-17 16:57:07 -0400

    Started work on ApplyNow widget.

--- page.php
+++ page.php
@@ -4,9 +4,8 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-4">
-			<?php
-				echo display_page_side_menu( $post->ID, TRUE );
-			?>
+			<?php echo display_page_side_menu( $post->ID, TRUE ); ?>
+			<?php if ( is_active_sidebar( 'sidebar' ) ) { dynamic_sidebar( 'sidebar' ); } ?>
 		</div>
 		<div class="col-md-8">
 			<h1><?php the_title(); ?></h1>
